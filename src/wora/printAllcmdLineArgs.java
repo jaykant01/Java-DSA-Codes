83f2a52b92907bb411a81277df948466e090e957
jaykant01 <jaykanty24092004@gmail.com>
2025-10-10 11:54:02 +0530

    made changes

---
+++ src/wora/printAllcmdLineArgs.java
@@ -0,0 +1,13 @@
+package wora;
+// Write a program to print all command line arguments in java
+public class printAllcmdLineArgs {
+    public static void main(String[] args) {
+        if(args.length > 0){
+            for(String value : args){
+                System.out.println(value);
+            }
+        }else{
+            System.out.println("No Arguments is passed");
+        }
+    }
+}
---
+++ src/wora/squareUsingcmdLineARGS.java
@@ -0,0 +1,8 @@
+package wora;
+
+public class squareUsingcmdLineARGS {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        System.out.println("Square of " + n + " is " + (n * n));
+    }
+}
